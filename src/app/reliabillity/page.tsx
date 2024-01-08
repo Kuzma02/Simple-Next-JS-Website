fa784b39c9b3e5216dbe2c5b3981e588c7adc35c
Kuzma02 <aleksandarkuzmanovic02@gmail.com>
2024-01-08 12:03:36 +0100

    finished project

---
+++ src/app/reliabillity/page.tsx
@@ -0,0 +1,10 @@
+import reliabilityImg from "@/../public/reliability.jpg";
+import Hero from "@/components/hero";
+
+export default function ReliabillityPage() {
+  return (
+    <>
+    <Hero imgData={reliabilityImg} imgAlt="welding" title="Super high reliabillity hosting" />
+    </>
+  );
+}
